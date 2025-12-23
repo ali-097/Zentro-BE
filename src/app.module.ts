a21a229782d8ebd2155f451624e46014ae9ef924
sultan3333 <hsra330@gmail.com>
2025-12-23 13:08:08 +0000

    Backend was dockerized Database was connected with the backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,       // MUST be "db"
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // dev only
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
