0eec165dff8e9d19672409ef9b0f8d874174724b
sultan3333 <hsra330@gmail.com>
2026-01-05 13:20:13 +0000

    comments removede in the app.module.ts file. The ports are all 5432 now to match the database port.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,4 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -18,13 +9,13 @@ import 'dotenv/config';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,       // MUST be "db"
+      host: process.env.DB_HOST,       
       port: Number(process.env.DB_PORT) || 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true, // dev only
+      synchronize: true, 
     }),
   ],
   controllers: [AppController],
